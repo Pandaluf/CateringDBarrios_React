20eae4d0bd35883cb13e7a878812ba2ce63a6b77
Pandaluf <juliobarrios0296@gmail.com>
2025-03-07 00:04:37 -0500

    feat: Price edited

--- src/pages/services/Services.tsx
+++ src/pages/services/Services.tsx
@@ -20,23 +20,23 @@ function Services() {
                              description="Dulzura en cada bocado. Nuestros minialfajores
                              son perfectos para disfrutar y compartir en cualquier momento.
                              ¡Pruébalos! ✨"
-                             price1="S/.50.00"
-                             price2="S/.30.00"
-                             price3="S/.15.00"
+                             price1="S/.70.00"
+                             price2="S/.40.00"
+                             price3="S/.20.00"
                 />
                 <CustomCard  img={imagesCard.littleEars1}
                              title="Mini Orejitas Dulces"
                              description="¡Crujientes, dulces y deliciosas! Nuestras Mini
                              Orejitas Dulces son el snack perfecto para cualquier momento. 🍪"
-                             price1="S/.60.00"
-                             price2="S/.35.00"
+                             price1="S/.80.00"
+                             price2="S/.40.00"
                              price3="S/.20.00"
                 />
                 <CustomCard  img={imagesCard.cheesePitipan1}
                              title="Petipan de Jamon y Queso"
                              description="¡Suave, dorado y relleno de sabor! Disfruta nuestro
                              Petipan de Jamón y Queso, perfecto para cualquier antojo. 🥪🧀✨"
-                             price1="S/.130.00"
+                             price1="S/.140.00"
                              price2="S/.70.00"
                              price3="S/.40.00"
                 />
@@ -44,7 +44,7 @@ function Services() {
                              title="Empanaditas de Queso"
                              description="¡Crujientes por fuera y queso derretido por dentro!
                              Nuestras Empanaditas de Queso son el bocado ideal para compartir y disfrutar. 🧀✨"
-                             price1="S/.100.00"
+                             price1="S/.110.00"
                              price2="S/.60.00"
                              price3="S/.35.00"
                 />
@@ -52,7 +52,7 @@ function Services() {
                              title="Empanaditas de Pollo"
                              description="¡Crujientes y llenas de sabor casero! Nuestras Empanaditas de Pollo son
                              el bocado perfecto para cualquier ocasión. 🍗✨"
-                             price1="S/.100.00"
+                             price1="S/.110.00"
                              price2="S/.60.00"
                              price3="S/.35.00"
                 />
@@ -60,7 +60,7 @@ function Services() {
                              title="Empanaditas de Carne"
                              description="¡Crujientes y jugosas! Nuestras Empanaditas de Carne están llenas de sabor
                               y perfectas para cualquier momento. 🥩✨"
-                             price1="S/.100.00"
+                             price1="S/.110.00"
                              price2="S/.60.00"
                              price3="S/.35.00"
                 />
@@ -68,7 +68,7 @@ function Services() {
                              title="Pitipan de Pollo"
                              description="¡Suave, sabroso y delicioso! Nuestro Pitipán de Pollo es el bocado ideal para
                              compartir y disfrutar en cualquier momento. 🥪✨"
-                             price1="S/.100.00"
+                             price1="S/.110.00"
                              price2="S/.60.00"
                              price3="S/.35.00"
                 />
@@ -76,8 +76,8 @@ function Services() {
                              title="Butifarra de Jamon con Salsa Criolla"
                              description="¡Sabor tradicional en cada bocado! Nuestra Butifarra de Jamón con Salsa Criolla
                              combina jamón y el toque irresistible de la salsa criolla. 🥪✨"
-                             price1="S/.110.00"
-                             price2="S/.60.00"
+                             price1="S/.120.00"
+                             price2="S/.65.00"
                              price3="S/.35.00"
                 />
                 <CustomCard  img={imagesCard.miniHamCheeseCorsillat1}
@@ -85,8 +85,8 @@ function Services() {
                              description="¡Pequeñas, crujientes y deliciosas! Nuestras Mini Corsilltas de Jamón y Queso
                              son el bocado perfecto para cualquier momento. 🧀🥪✨"
                              price1="S/.160.00"
-                             price2="S/.70.00"
-                             price3="S/.5000"
+                             price2="S/.80.00"
+                             price3="S/.40.00"
                 />
                 <CustomCard  img={imagesCard.miniHotdogRoll1}
                              title="Mini Enrrollado de Hotdog"
@@ -100,7 +100,7 @@ function Services() {
                              title="Mini Triple de Palta y Tomate"
                              description="¡Fresco, cremoso y delicioso! Nuestro Mini Triple de Palta y Tomate es el bocado
                              perfecto para cualquier ocasión. 🥑🍅✨"
-                             price1="S/.90.00"
+                             price1="S/.100.00"
                              price2="S/.50.00"
                              price3="S/.30.00"
                 />
@@ -108,7 +108,7 @@ function Services() {
                              title="Mini Triple de Pollo"
                              description="¡Suaves, frescos y deliciosos! Nuestro Mini Triple de Pollo es el bocado perfecto
                              para compartir y disfrutar en cualquier ocasión. 🥪✨"
-                             price1="S/.100.00"
+                             price1="S/.110.00"
                              price2="S/.60.00"
                              price3="S/.35.00"
                 />
@@ -116,7 +116,7 @@ function Services() {
                              title="Mini Triple de Huevo y Aceituna"
                              description="¡Suaves, cremosos y llenos de sabor! Nuestro Mini Triple de Huevo y Aceituna es el
                              bocado perfecto para disfrutar en cualquier momento. 🥪✨"
-                             price1="S/.100.00"
+                             price1="S/.110.00"
                              price2="S/.60.00"
                              price3="S/.35.00"
                 />
